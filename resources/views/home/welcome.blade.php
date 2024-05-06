ab6f0c2833eabd917e37a51bd26e5e0dcddc8b42
Baguettou <68337546+Baguettou@users.noreply.github.com>
2024-05-06 00:30:29 -0500

    Fixed name

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -43,7 +43,7 @@
             </div>
         </div>
         <div class="col m-4 d-flex flex-column align-items-center justify-content-center">
-            <h1 class="text-center">¡Bienvenid@ a LockDock!</h1>
+            <h1 class="text-center">¡Bienvenid@ a LockDoc!</h1>
             <p class="text-lg text-center">
                 Aquí puedes subir y acceder de manera <strong>segura</strong> a tus documentos PDF sensibles. Utilizamos Auth0 para autenticación, contamos con encriptación de datos propia y una interfaz intuitiva para una experiencia confiable y fácil de usar. ¡Explora la seguridad en la gestión de documentos con nosotros!            
             </p>
@@ -52,12 +52,12 @@
 
     <div class="col m-5">
         <div class="col-lg-12">
-            <h3>¿Cómo empezó LockDock?</h3>
+            <h3>¿Cómo empezó LockDoc?</h3>
             <p class="text">
-                LockDock surgió como respuesta a la creciente necesidad de proporcionar a los usuarios una plataforma segura para la gestión de documentos PDF sensibles. En un entorno donde la privacidad y la seguridad de la información son de suma importancia, se identificó una problemática significativa: la ausencia de un lugar confiable donde los usuarios pudieran almacenar y acceder a sus documentos de manera segura. Ante esta situación, se decidió crear LockDock con el objetivo de ofrecer una solución integral y confiable.
+                LockDoc surgió como respuesta a la creciente necesidad de proporcionar a los usuarios una plataforma segura para la gestión de documentos PDF sensibles. En un entorno donde la privacidad y la seguridad de la información son de suma importancia, se identificó una problemática significativa: la ausencia de un lugar confiable donde los usuarios pudieran almacenar y acceder a sus documentos de manera segura. Ante esta situación, se decidió crear  con el objetivo de ofrecer una solución integral y confiable.
             </p>
             <p class="text">
-                LockDock se creó utilizando las últimas tecnologías en seguridad informática, como Auth0 para la autenticación y técnicas avanzadas de encriptación de datos. El proceso de desarrollo se llevó a cabo con un enfoque centrado en el usuario, priorizando la facilidad de uso y la seguridad en cada etapa del diseño y la implementación.
+                LockDoc se creó utilizando las últimas tecnologías en seguridad informática, como Auth0 para la autenticación y técnicas avanzadas de encriptación de datos. El proceso de desarrollo se llevó a cabo con un enfoque centrado en el usuario, priorizando la facilidad de uso y la seguridad en cada etapa del diseño y la implementación.
             </p>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LockDock</title>
+    <title>LockDoc</title>
     <!-- CSS, JavaScript, or other resources -->
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -13,7 +13,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="ms-4 navbar-brand" href="/">LockDock</a>
+                <a class="ms-4 navbar-brand" href="/">LockDoc</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -42,8 +42,8 @@
 
     <footer class="footer mt-auto py-3 bg-light">
         <div class="container">
-            <p class="m-0"><strong>&copy;2024 LockDock.</strong></p>
-            <p class="m-0"><small>Todos los derechos reservados. LockDock™ es una marca registrada de <strong>GitChest.</strong></small></p>
+            <p class="m-0"><strong>&copy;2024 LockDoc.</strong></p>
+            <p class="m-0"><small>Todos los derechos reservados. LockDoc™ es una marca registrada de <strong>GitChest.</strong></small></p>
         </div>
     </footer>
 
